ac1a28ff93d5c71c5f8a3f1f33df2288642d05be
Srikanth Kollimerla <srikanthkollimerla@MacBook-Pro-di-Srikanth.local>
2025-02-24 00:16:36 -0600

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,10 @@ public class Main {
         System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input + ": command not found");
+        while(true) {
+            String input = scanner.nextLine();
+            System.out.println(input + ": command not found");
+        }
 
     }
 }
