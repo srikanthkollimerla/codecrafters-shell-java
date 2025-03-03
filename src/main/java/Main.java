3d39a9a3f14633984e4f70e9559c4a1f85a6d260
srikanthkollimerla <srikanthkollimerla@gmail.com>
2025-03-02 23:47:05 -0600

    error handling

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
+
+        Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.print("$ ");
-            Scanner scanner = new Scanner(System.in);
+            //System.out.print("$ ");
             String input = scanner.nextLine();
             if (input.equals("exit 0"))
                 break;
