ba47814a3ee6958ade5ab4809bd9c3ab6d649be2
srikanthkollimerla <srikanthkollimerla@gmail.com>
2025-03-02 23:45:37 -0600

    error handling

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,20 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
-        System.out.print("$ ");
-
-        Scanner scanner = new Scanner(System.in);
-
-        String input = scanner.nextLine();
-        do {
-            System.out.println(input + ": command not found");
+        while (true) {
             System.out.print("$ ");
-            input = scanner.nextLine();
-            if(input.equals("exit")) {
-                System.exit(0);
-            }
-        } while (!input.matches(""));
-        scanner.close();
-
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (input.equals("exit 0"))
+                break;
+            System.out.println(input + ": command not found");
+        }
     }
 }
