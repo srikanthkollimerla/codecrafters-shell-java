6bfaa5ab589b9f11fb1a40f8ea92527670f5a2bf
srikanthkollimerla <srikanthkollimerla@gmail.com>
2025-03-02 23:47:51 -0600

    error handling

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            //System.out.print("$ ");
+            System.out.print("$ ");
             String input = scanner.nextLine();
             if (input.equals("exit 0"))
                 break;
