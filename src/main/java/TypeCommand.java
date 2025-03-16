f9c86f9345cf4191033bde5fc6eeba5cc1334332
srikanthkollimerla <srikanthkollimerla@gmail.com>
2025-03-16 01:20:48 -0500

    type command extension to serch Path

--- src/main/java/TypeCommand.java
+++ src/main/java/TypeCommand.java
@@ -1,11 +1,33 @@
+import java.io.File;
+
 public class TypeCommand implements Command {
     @Override
     public void execute(String input) {
-        String commandName = input.substring(5);
+        String commandName = input.substring(5).trim();
         if (CommandRegistry.hasCommand(commandName)) {
             System.out.println(commandName + " is a shell builtin");
-        } else {
+            return;
+        }
+        String path = findExecutableInPath(commandName);
+        if (path != null) {
+            System.out.println(commandName + " is " + path);
+        }
+        else {
             System.out.println(commandName + ": not found");
         }
+    }
+
+    private String findExecutableInPath(String command) {
+        String pathEnv = System.getenv("PATH");
+        if (pathEnv == null || pathEnv.isEmpty()) return null;
+
+        String[] paths = pathEnv.split(":");
+        for (String dir : paths) {
+            File file = new File(dir, command);
+            if (file.exists() && file.canExecute()) {
+                return file.getAbsolutePath();
+            }
+        }
+        return null;
     }
 }
