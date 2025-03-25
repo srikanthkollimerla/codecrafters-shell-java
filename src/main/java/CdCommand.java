42d1af364452a1ff08dd90d94baed3010c7e2b22
srikanthkollimerla <srikanthkollimerla@gmail.com>
2025-03-24 22:35:45 -0500

    relative paths support for Cd command

--- src/main/java/CdCommand.java
+++ src/main/java/CdCommand.java
@@ -1,9 +1,21 @@
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class CdCommand implements Command{
     @Override
     public void execute(String input) {
         String newPath = input.split(" ")[1];
+        if(newPath.equals("./"))
+            return;
+        else if(newPath.startsWith("../")){
+            Path path = Paths.get(ShellContext.getCurrentPath()).resolve(newPath).normalize();
+            ShellContext.setCurrentPath(path.toString());
+            return;
+        }
+        else if(newPath.startsWith("./"))
+            newPath = ShellContext.getCurrentPath() + newPath.replaceFirst("^\\./", "/"); //manual way
+
         File newDir = new File(newPath);
         if (!newDir.isDirectory()) {
             System.out.println("cd: " + newPath + ": No such file or directory");
@@ -12,4 +24,21 @@ public class CdCommand implements Command{
         // Change the working directory
        ShellContext.setCurrentPath(newPath);
     }
+
+    //manual way to resolve the path.
+    /*
+    private String resolve(String dir) {
+        String[] input = dir.split("/");
+        int count = 0;
+        for(String curr : input) {
+            if(curr.equals(".."))
+                count++;
+        }
+        File file = new File(ShellContext.getCurrentPath());
+        while(count > 0) {
+            file = file.getParentFile();
+            count--;
+        }
+        return file.getAbsolutePath();
+    }*/
 }
